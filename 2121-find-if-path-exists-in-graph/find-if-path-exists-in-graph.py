a0c4c182885a53ce95b9b07daec5baac0124cb51
dura12 <124529996+dura12@users.noreply.github.com>
2025-07-31 16:09:21 +0300

    Time: 357 ms (70.44%) | Memory: 96.6 MB (77.33%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -4,15 +4,26 @@ class Solution:
         for u , v in edges:
             graph[v].append(u)
             graph[u].append(v)
-        visit = set()
-        def dfs(node):
-            visit.add(node)
+        # visit = set()
+        # def dfs(node):
+        #     visit.add(node)
+        #     if node == destination:
+        #         return True
+        #     for nei in graph[node]:
+        #         if nei not in visit:
+        #             if dfs(nei):
+        #                 return True
+        #     return False
+        # return dfs(source)
+        stack = [source]
+        visit = set([source])
+        while stack:
+            node = stack.pop()
             if node == destination:
                 return True
             for nei in graph[node]:
                 if nei not in visit:
-                    if dfs(nei):
-                        return True
-            return False
-        return dfs(source)
-        
+                    stack.append(nei)
+                    visit.add(nei)
+        return False
+                
