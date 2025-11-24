d63ab501826e591f095d9d555651952c11462cf5
dura12 <124529996+dura12@users.noreply.github.com>
2025-11-24 10:00:00 +0300

    Time: 8 ms (14.02%) | Memory: 19.5 MB (10.40%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,22 +1,21 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        start = 0
         end = len(graph) - 1
         ans = []
-        seen =set()
-        def dfs(node , path):
+        
+        def dfs(node, path):
+            # Base Case: If we reached the destination node
             if node == end:
-                ans.append(path.copy())
+                ans.append(list(path)) # Make a copy of the path and add to answers
                 return 
-            seen.add(node)
-            for nei in graph[node]:
-                if nei not in seen:
-                    path.append(nei)
-                    dfs(nei , path)
-                    path.pop()
-            seen.remove(node)
-            return 
-        dfs(0 , [0])
-        return ans
             
-                    
+            # Recursive Step
+            for nei in graph[node]:
+                # Proceed to neighbors
+                path.append(nei)
+                dfs(nei, path)
+                path.pop() # Backtrack: Remove the neighbor to try the next one
+        
+        # Start DFS from node 0, with [0] as the initial path
+        dfs(0, [0])
+        return ans
