cea6ae22dc8f6eb7606a2a8eeb8fa13b3ee93289
dura12 <124529996+dura12@users.noreply.github.com>
2025-11-24 12:01:27 +0300

    Time: 3 ms (83.07%) | Memory: 21.6 MB (27.54%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,44 +1,25 @@
-class Node:
-    def __init__(self , val):
-        self.val = val
-        self.next = None
-        self.prev = None
 class MinStack:
 
     def __init__(self):
-        self.stack = []
-        self.head , self.tail = Node(0) , Node(0)
-        self.head.next , self.tail.prev = self.tail , self.head
-    def push(self, val: int) -> None:
-        node = Node(val)
-        self.insert(node)
-        self.stack.append(node)
-    def insert(self, node):
-        prev , nxt = self.tail.prev  ,self.tail
-        if prev.val > node.val:
-            prev.next = self.tail.prev = node
-            node.prev , node.next = prev , nxt
-        else:
-            prev , nxt = self.head ,   self.head.next
-            prev.next = nxt.prev = node
-            node.next , node.prev = nxt , prev
-    def remove(self , node):
-        prev , nxt = node.prev , node.next
-        prev.next , nxt.prev = nxt , prev
-    def pop(self) -> None:
-        if not self.stack:
-            return -1
-        node = self.stack.pop()
-        self.remove(node)
-        return node.val
+        self.stack = [[0 , float("inf")]]
 
-    def top(self) -> int:
-        node = self.stack[-1]
-        return node.val
+    def push(self, val: int) -> None:
+        self.stack.append([val , min(self.stack[-1][1] , val)])
+    def pop(self) -> None:
+        if len(self.stack) < 2:
+            raise Exception("empty list")
+        self.stack.pop()
         
 
+    def top(self) -> int:
+        if len(self.stack) < 2:
+            raise Exception("empty list")
+        return self.stack[-1][0]
+
     def getMin(self) -> int:
-        return self.tail.prev.val
+        if len(self.stack) < 2:
+            raise Exception("empty list")
+        return self.stack[-1][1]
         
 
 
